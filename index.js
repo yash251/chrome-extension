a78e193fca814efd8c41a480946b10fe9cf4a428
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-19 19:45:06 +0530

    another method for li

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
 });
 
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-}
+    listItems += "<li>" + myLeads[i] + "</li>";
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+};
+
+ulEl.innerHTML = listItems; // DOM Manipulation comes at a cost so instead of running innerHTML inside a for loop, we store it in a variable and then use innerHTML outside the loop.
