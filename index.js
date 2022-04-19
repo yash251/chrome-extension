ed8cacd133f0039e3b5c6957b8d3867142534819
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-19 19:02:39 +0530

    push lead to array

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push("www.yashjagtap.com");
+    console.log(myLeads);
 });
